744025e0b18e954daf412c6bc33aafddd03576a7
Kevin <Kevin@DESKTOP-12A507O>
2018-11-24 19:12:52 +0000

    Image rec file started work with intellij

---
+++ digitrec.py
@@ -0,0 +1,26 @@
+import keras as kr
+import gzip
+import numpy as np
+import sklearn.preprocessing as pre
+
+# Start a neural network, building it by layers.
+model = kr.models.Sequential()
+
+# Add a hidden layer with 1000 neurons and an input layer with 784.
+model.add(kr.layers.Dense(units=1000, activation='relu', input_dim=784))
+# Add a three neuron output layer.
+model.add(kr.layers.Dense(units=10, activation='softmax'))
+
+# Build the graph.
+model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+
+with gzip.open('data/train-images-idx3-ubyte.gz', 'rb') as f:
+    train_img = f.read()
+
+with gzip.open('data/train-labels-idx1-ubyte.gz', 'rb') as f:
+    train_lbl = f.read()
+    
+train_img = ~np.array(list(train_img[16:])).reshape(60000, 28, 28).astype(np.uint8) / 255.0
+train_lbl =  np.array(list(train_lbl[ 8:])).astype(np.uint8)
+
+inputs = train_img.reshape(60000, 784)
